79df97d94be9345278560d59cfb91708b04e4624
AYUSH KUMAR SINGH <ab49ayush@gmail.com>
2026-07-02 10:15:11 +0530

    Time: 7 ms (85.53%) | Memory: 19.3 MB (79.48%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -2,15 +2,15 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1 or numRows >= len(s):
             return s
-
-        rows = [''] * numRows
-        cur_row = 0
-        going_down = False
-
-        for c in s:
-            rows[cur_row] += c
-            if cur_row == 0 or cur_row == numRows - 1:
-                going_down = not going_down
-            cur_row += 1 if going_down else -1
-
-        return ''.join(rows)
+            
+        res = []
+        cycle_len = 2 * numRows - 2
+        n = len(s)
+        
+        for i in range(numRows):
+            for j in range(0, n - i, cycle_len):
+                res.append(s[j + i])
+                if i != 0 and i != numRows - 1 and j + cycle_len - i < n:
+                    res.append(s[j + cycle_len - i])
+                    
+        return "".join(res)
