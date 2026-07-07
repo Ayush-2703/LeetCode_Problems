9ab53d07cf93e5ef9b87b0cdba9613330e526647
AYUSH KUMAR SINGH <ab49ayush@gmail.com>
2026-07-07 11:02:41 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (85.87%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -1,18 +1,9 @@
 class Solution:
     def sumAndMultiply(self, n: int) -> int:
-        rev_x = 0
-        digit_sum = 0
-        temp = n
-        while temp > 0:
-            d = temp % 10
-            if d != 0:
-                rev_x = rev_x * 10 + d
-                digit_sum += d
-            temp //= 10
-
-        x = 0
-        while rev_x > 0:
-            x = x * 10 + rev_x % 10
-            rev_x //= 10
-
-        return x * digit_sum
+        s = str(n)
+        non_zeros = [c for c in s if c != '0']
+        if not non_zeros:
+            return 0
+            
+        x = int("".join(non_zeros))
+        return x * sum(int(c) for c in non_zeros)
