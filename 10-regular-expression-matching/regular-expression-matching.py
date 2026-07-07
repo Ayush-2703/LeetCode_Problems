661acbf8166178669af592af0e1d3a80dcf4ca26
AYUSH KUMAR SINGH <ab49ayush@gmail.com>
2026-07-07 11:07:09 +0530

    Time: 7 ms (58.59%) | Memory: 19.3 MB (70.28%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -2,23 +2,21 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
 
         m, n = len(s), len(p)
-
-        def matches(si: int, pj: int) -> bool:
-            return p[pj] == '.' or p[pj] == s[si]
-
-        prev = [False] * (n + 1)
-        prev[0] = True
-        for j in range(1, n + 1):
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        
+        dp[0][0] = True
+        
+        for j in range(2, n + 1):
             if p[j - 1] == '*':
-                prev[j] = prev[j - 2]
-
+                dp[0][j] = dp[0][j - 2]
+                
         for i in range(1, m + 1):
-            curr = [False] * (n + 1)
             for j in range(1, n + 1):
-                if p[j - 1] == '*':
-                    curr[j] = curr[j - 2] or (matches(i - 1, j - 2) and prev[j])
-                else:
-                    curr[j] = matches(i - 1, j - 1) and prev[j - 1]
-            prev = curr
-
-        return prev[n]        
+                if p[j - 1] == '.' or p[j - 1] == s[i - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 2]
+                    if p[j - 2] == '.' or p[j - 2] == s[i - 1]:
+                        dp[i][j] = dp[i][j] or dp[i - 1][j]
+                        
+        return dp[m][n]
